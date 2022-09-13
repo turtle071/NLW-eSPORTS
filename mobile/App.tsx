c22d072cab880ef452349ab977d49f9ef585b18c
Turtle <emanuelsadhot@gmail.com>
2022-09-12 22:19:23 -0300

    Update App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string,
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aaaaaaaaaaaaa</Text>
+
+      <Text>
+        OLA
+      </Text>
+
+      <Button title="Send 1" />
+
       <StatusBar style="auto" />
     </View>
   );
+}
+
+function Button(props: ButtonProps) {
+  return(
+  <TouchableOpacity>
+    <Text>{props.title}</Text>
+  </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
